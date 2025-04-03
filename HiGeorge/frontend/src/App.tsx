881c0b3ee693b8f01b343241bde560650bd8b3cf
amvdevlab <amvdevlab@gmail.com>
2025-04-02 23:28:46 -0400

    commit and push updates to branch

--- HiGeorge/frontend/src/App.tsx
+++ HiGeorge/frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import Chatbox from './components/chatbox';
+import smile from './assets/smile.png';
 import './App.css';
 
 function App() {
     return (
         <div className="app">
             <main className="app-main">
+                <img src={smile} alt="Smiling Face" className="app-smile" />
                 <h1 className="app-title">Hi, I'm George! How can I help you today?</h1>
                 <Chatbox />
             </main>
--- HiGeorge/frontend/src/components/chat.tsx
+++ HiGeorge/frontend/src/components/chat.tsx
@@ -27,13 +27,13 @@ const Chatbox = () => {
                     <svg
                         width="20"
                         height="20"
-                        viewBox="0 0 24 24"
+                        viewBox="0 0 20 20"
                         fill="none"
                         xmlns="http://www.w3.org/2000/svg"
-                        className="arrow-icon"
+                        className="send-icon"
                     >
                         <path
-                            d="M5 12H19M19 12L12 5M19 12L12 19"
+                            d="M4.16666 10H15.8333M15.8333 10L10 4.16669M15.8333 10L10 15.8334"
                             stroke="currentColor"
                             strokeWidth="2"
                             strokeLinecap="round"
